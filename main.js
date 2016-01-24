9433352d2609432b51d6e4015d92868245966a9b
Anthony Lackey <alackey96@gmail.com>
2016-01-24 08:26:27 -0800

    v1 - All main functions working

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
+var tabUrls = {};
+var bingUrlRemoveFromList = -1;
 
 //Waits for user to search for something in the Search Bar
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
-  var url = details.url.replace("www.google.com/search", "www.bing.com/search");
-  console.log('Google to Bing url: ' + url);
+    var url = details.url.replace("www.google.com/search", "www.bing.com/search");
 
-  //Checks if there are 2 search queries
-  var moddedURL = url.split('&');
-  if (moddedURL[moddedURL.length - 1].includes('#q=')){
-      url = toOneQuery(moddedURL);
-  }
+    //Checks if there are 2 search queries
+    var moddedURL = url.split('&');
+    if (moddedURL[moddedURL.length - 1].includes('#q=')){
+        url = toOneQuery(moddedURL);
+    }
 
-  openTab(url);
-  closeTab(url);
+    bingUrlRemoveFromList = url;
+
+    openTab(url);
+    closeTab(url);
 }, {urls: ["*://www.google.com/search*"]}, ["blocking"] );
+
+chrome.storage.local.set({'RecentlyClosed': []});
 
 function toOneQuery(moddedURL){
     //Replace the 1st "wrong" query with second
@@ -26,24 +31,87 @@ function toOneQuery(moddedURL){
         newURL += ('&' + moddedURL[i]);
     }
 
-    //Add ie query(ex. UTF-8)
+    //Add ie query(ex. ie=UTF-8)
     newURL += moddedURL[moddedURL.length - 1].split('#')[0];
 
     return newURL;
 }
 
 function openTab(url){
-  chrome.tabs.create({'url': url, 'selected': false});
+    chrome.tabs.create({'url': url, 'selected': false});
 }
 
 function closeTab(url){
-  chrome.tabs.query({ url: url}, function (tabs) {
+    chrome.tabs.query({ url: url}, function (tabs) {
 
-    //Calls closeTab till opened tab is done loading
-    if (tabs[0].status == "loading") {
-      closeTab(url);
-    } else {
-      chrome.tabs.remove(tabs[0].id);
-    };
-  });
+        //Calls closeTab till opened tab is done loading
+        if (tabs[0].status == "loading") {
+            closeTab(url);
+        } else {
+            chrome.tabs.remove(tabs[0].id);
+        }
+
+    });
 }
+
+//Remove tabs opened when computer wakes from sleep
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+        chrome.tabs.remove(details.tabId);
+    }, {urls: ["https://www.bing.com/searchdomaincheck?format=domain&type=chrome"]}
+);
+
+//Open Recently Closed Tab
+chrome.commands.onCommand.addListener(function(command) {
+    chrome.storage.local.get('RecentlyClosed', function (recentlyClosedStorage) {
+
+        if (Object.getOwnPropertyNames(recentlyClosedStorage.RecentlyClosed).length === 1){
+            //If our Recently Closed list is empty
+            chrome.sessions.getRecentlyClosed( function (recentlyClosedSession) {
+                for (var tab of recentlyClosedSession){
+                    recentlyClosedStorage.RecentlyClosed.unshift(tab.tab.url);
+                }
+                saveThenOpen(recentlyClosedStorage.RecentlyClosed);
+            });
+        } else {
+            //Open Next Tab
+            saveThenOpen(recentlyClosedStorage.RecentlyClosed);
+        }
+
+    });
+});
+
+function saveThenOpen(RecentlyClosed) {
+    var url = RecentlyClosed.pop();
+    chrome.storage.local.set({'RecentlyClosed': RecentlyClosed});
+    chrome.tabs.create({'url': url, 'selected': true});
+}
+
+//Adds closed tab to Recently Closed tabs
+chrome.tabs.onRemoved.addListener(function (tabID, info) {
+
+    if (tabUrls[tabID] == 'remove') {
+        delete tabUrls[tabID];
+    } else {
+        chrome.storage.local.get('RecentlyClosed', function (recentlyClosedStorage) {
+            recentlyClosedStorage.RecentlyClosed.push(tabUrls[tabID]);
+            delete tabUrls[tabID];
+            chrome.storage.local.set({'RecentlyClosed': recentlyClosedStorage.RecentlyClosed});
+        });
+    }
+
+});
+
+chrome.tabs.onUpdated.addListener(function (tabID, tabInfo) {
+
+    //Update URL for a current or new tab
+    if (tabInfo.hasOwnProperty('url')) {
+        if (bingUrlRemoveFromList != tabInfo.url) {
+            tabUrls[tabID] = tabInfo.url;
+        } else {
+            //Tab with Bing Search
+            bingUrlRemoveFromList = -1;
+            tabUrls[tabID] = 'remove';
+        }
+    }
+
+});
